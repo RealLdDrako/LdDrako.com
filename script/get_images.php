6513ec1853a147932adb600524cd6ff8d7764652
RealLdDrako <RealLdDrako@gmamil.com>
2024-07-15 03:07:55 -0400

    1.0

--- script/get_images.php
+++ script/get_images.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
 $imageFolder = '../images/AroundTheVerse';
 $imageFiles = scandir($imageFolder);
 $validImageFiles = array_filter($imageFiles, function ($file) {
