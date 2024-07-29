90dc94564044072b8f600f516bc1691b59ec17f0
RealLdDrako <RealLdDrako@gmamil.com>
2024-07-29 00:06:51 -0400

    1.0

--- script/get_images.php
+++ script/get_images.php
@@ -3,12 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-
 $imageFolder = '../images/AroundTheVerse';
-$imageFiles = scandir($imageFolder);
-$validImageFiles = array_filter($imageFiles, function ($file) {
-    return preg_match('/\.(jpg|jpeg|png|gif)$/i', $file);
-});
-echo json_encode($validImageFiles);
+
+if (is_dir($imageFolder) && is_readable($imageFolder)) {
+    $imageFiles = scandir($imageFolder);
+    $validImageFiles = array_filter($imageFiles, function ($file) {
+        return preg_match('/\.(jpg|jpeg|png|gif)$/i', $file);
+    });
+    echo json_encode($validImageFiles);
+} else {
+    echo json_encode(array("error" => "Directory not found or not readable."));
+}
 ?>
