40f628da9bc467462adbbe2b2fa7e4f6ea033cdd
RealLdDrako <RealLdDrako@gmamil.com>
2024-07-29 00:19:47 -0400

    1.0

--- script/get_images.php
+++ script/get_images.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$imageFolder = '../images/AroundTheVerse';
+$imageFolder = '../images/AroundVerse';
 
 if (is_dir($imageFolder) && is_readable($imageFolder)) {
     $imageFiles = scandir($imageFolder);
